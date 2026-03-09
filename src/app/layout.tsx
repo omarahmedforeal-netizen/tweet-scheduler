23bf98eb061bef44fdbd07b941eee08ae3d52c38
omarahmedforeal-netizen <omarahmedforeal@gmail.com>
2026-03-09 23:28:50 +0300

    add app layout and API routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,10 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: '\u0645\u062C\u062F\u0648\u0644 \u0627\u0644\u062A\u063A\u0631\u064A\u062F\u0627\u062A | Tweet Scheduler',
-  description: '\u062C\u062F\u0648\u0644 \u062A\u063A\u0631\u064A\u062F\u0627\u062A\u0643 \u0628\u0627\u0644\u0639\u0631\u0628\u064A\u0629 \u0628\u0633\u0647\u0648\u0644\u0629',
+  title: 'مجدول التغريدات | Tweet Scheduler',
+  description: 'جدول تغريداتك بالعربية بسهولة',
   icons: {
-    icon: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">\uD83D\uDC26</text></svg>',
+    icon: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">🐦</text></svg>',
   },
 }
 
