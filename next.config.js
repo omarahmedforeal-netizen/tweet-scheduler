d70c23365a9414fb6949730e4c54a9f9e8ca03a7
omarahmedforeal-netizen <omarahmedforeal@gmail.com>
2026-03-10 01:54:49 +0300

    fix: simplify next.config.js - remove Terser workaround (i18n approach handles encoding)

--- next.config.js
+++ next.config.js
@@ -1,29 +1,7 @@
 /** @type {import('next').NextConfig} */
-const TerserPlugin = require('terser-webpack-plugin')
-
 const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['twitter-api-v2']
-  },
-  swcMinify: false,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.output = config.output || {}
-      config.output.charset = true
-
-      config.optimization = config.optimization || {}
-      config.optimization.minimizer = [
-        new TerserPlugin({
-          terserOptions: {
-            output: {
-              ascii_only: false,
-              utf8: true,
-            },
-          },
-        }),
-      ]
-    }
-    return config
   }
 }
 
