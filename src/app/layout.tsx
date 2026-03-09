c7e6abfa96e1342c5a046aecfb6d3aa0be366e86
omarahmedforeal-netizen <omarahmedforeal@gmail.com>
2026-03-10 01:27:44 +0300

    fix: use safe favicon character encoding in layout metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
   title: 'مجدول التغريدات | Tweet Scheduler',
   description: 'جدول تغريداتك بالعربية بسهولة',
   icons: {
-    icon: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">\uD83D\uDC26</text></svg>',
+    icon: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">&#128038;</text></svg>',
   },
 }
 
